c4b8fccffcfac923e8143bfb3249bb96061fee46
Rubens Rodrigues <104909014+rubens-juniorr@users.noreply.github.com>
2024-10-01 15:54:29 -0300

    Feat: Avatar List Component

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,32 @@
-import { CardComponent, Header } from '@/componets'
+import { AvatarList, CardComponent, Header } from '@/componets'
 import { Container } from '@mui/material'
+import { currencConverter } from '@/uils'
 function Home() {
+  const mockListData = [
+    {
+      avatar: '/dnc-avatar.svg',
+      name: 'Nome Sobrenome 1',
+      subtitle: currencConverter(5254.74),
+    },
+    {
+      avatar: '/dnc-avatar.svg',
+      name: 'Nome Sobrenome 2',
+      subtitle: currencConverter(56056.75),
+    },
+    {
+      avatar: '/dnc-avatar.svg',
+      name: 'Nome Sobrenome 3',
+      subtitle: currencConverter(2626555.528),
+    },
+  ]
   return (
     <>
       <Header />
       <Container maxWidth="lg">
         <CardComponent>CARD</CardComponent>
+        <CardComponent>
+          <AvatarList listData={mockListData} />
+        </CardComponent>
       </Container>
     </>
   )
--- src/types/index.ts
+++ src/types/index.ts
@@ -1,3 +1,4 @@
+export * from './avatarList'
 export * from './theme'
 export * from './typographies'
 export * from './formComponent'
