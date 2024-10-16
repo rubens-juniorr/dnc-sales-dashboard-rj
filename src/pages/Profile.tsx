a73bc0af2c37c4a6faa0c6645ea233dc0b0a645a
Rubens Rodrigues <104909014+rubens-juniorr@users.noreply.github.com>
2024-10-16 15:08:01 -0300

    Feat: Logout Flow

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -1,18 +1,40 @@
 import { useContext } from 'react'
-import { CardComponent, Header, StyledButton } from '@/componets'
 import { AppThemeContext } from '@/contexts/AppThemeContext'
+
+//COMPONENTS
+import { CardComponent, Header, StyledH2, StyledButton } from '@/componets'
+import { Container, Grid } from '@mui/material'
+
+//SERVICES
+import { logout } from '@/services'
 
 function Profile() {
   const themeContext = useContext(AppThemeContext)
   return (
     <>
       <Header />
-      <CardComponent>
-        <StyledButton className="primary" onClick={themeContext?.toggleTheme}>
-          Trocar para tema{' '}
-          {themeContext?.appTheme === 'light' ? 'escuro' : 'claro'}
-        </StyledButton>
-      </CardComponent>
+      <Container className="mb-2" maxWidth="lg">
+        <Grid container spacing={4}>
+          <Grid item xs={12} sm={6}>
+            Seus Dados...
+          </Grid>
+          <Grid item xs={12} sm={6}>
+            <CardComponent>
+              <StyledH2 className="mb-1">Definições de conta</StyledH2>
+              <StyledButton
+                className="primary mb-1"
+                onClick={themeContext?.toggleTheme}
+              >
+                Trocar para tema{' '}
+                {themeContext?.appTheme === 'light' ? 'escuro' : 'claro'}
+              </StyledButton>
+              <StyledButton className="alert" onClick={logout}>
+                Logout
+              </StyledButton>
+            </CardComponent>
+          </Grid>
+        </Grid>
+      </Container>
     </>
   )
 }
